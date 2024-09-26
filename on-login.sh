cbefa21f1fc61205081dd15dae25d41db1c65735
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-09-26 12:56:03 -0400

    Update on-login.sh

--- on-login.sh
+++ on-login.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# Get the username of the logged-in user
-USERNAME=$(whoami)
+echo "Executing on-login script..."  # Debugging line
 
-# Start or ensure the user's container is running
-/opt/hivelab/manage_container.sh $USERNAME start
+if [ "\$SSH_ORIGINAL_COMMAND" == "bypass" ]; then
+    exec \$SHELL
+    exit 0
+fi
+
+USERNAME=\$(whoami)
+
+echo "Starting container for user: \$USERNAME"  # Debugging line
+
+/opt/hivelab/manage_container.sh \$USERNAME start
 
 # Get the container ID
-CONTAINER_ID=$(docker ps --filter name=hivelab-$USERNAME --format '{{.ID}}')
+CONTAINER_ID=\$(docker ps --filter name=hivelab-\$USERNAME --format '{{.ID}}')
 
-if [ -z "$CONTAINER_ID" ]; then
+if [ -z "\$CONTAINER_ID" ]; then
     echo "Failed to start or find your HiveLab container. Please contact support."
     exit 1
 fi
 
+echo "Executing interactive session in container..."  # Debugging line
+
 # Execute an interactive bash session in the user's container
-exec docker exec -it -e TERM=$TERM -e LANG=$LANG -u vscode $CONTAINER_ID /bin/bash -l
+exec docker exec -it -e TERM=\$TERM -e LANG=\$LANG -u vscode \$CONTAINER_ID /bin/bash -l
