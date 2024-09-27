d29306e0db39eb771771bce5b5168fe18d4dded4
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-09-26 22:48:35 -0400

    Update on-login.sh

--- on-login.sh
+++ on-login.sh
@@ -7,7 +7,13 @@ fi
 
 USERNAME=$(whoami)
 
-echo "Loggin in ${USERNAME}"
+echo "🐝 Welcome to Hive-Lab 🐝"
+echo "📦 Version 1.2.0!"
+echo "🤝 Contribute at: https://github.com/tristanpoland/Hive-Lab"
+echo "🔒 Loggin in as: ${USERNAME}"
+
+
+echo "🍯 Making sure your nest is running..."
 
 # Start or ensure the user's container is running
 /opt/hivelab/manage_container.sh $USERNAME start
@@ -16,9 +22,10 @@ echo "Loggin in ${USERNAME}"
 CONTAINER_ID=$(docker ps --filter name=hivelab-$USERNAME --format '{{.ID}}')
 
 if [ -z "$CONTAINER_ID" ]; then
-    echo "Failed to start or find your HiveLab container. Please contact support."
+    echo "Failed to start or find your HiveLab container. Please contact an adminstrator for support."
     exit 1
 fi
 
 # Execute an interactive bash session in the user's container
+echo "🌐 connecting you to your nest..."
 exec docker exec -it -e TERM=$TERM -e LANG=$LANG -u  $USERNAME $CONTAINER_ID /bin/bash -l
